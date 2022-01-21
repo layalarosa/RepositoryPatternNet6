9c17a582f2c29b2d06ea3d1677687433938603f0
layalarosa <layalarosa@gmail.com>
2022-01-21 14:13:37 -0200

    Integracion de los controladores para las llamadas a la base de datos.

---
+++ RepositoryPatternNet6/Controllers/BookController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RepositoryPatternNet6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+    }
+}
